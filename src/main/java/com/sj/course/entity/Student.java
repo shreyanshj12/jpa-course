500d895437eca8fbf566c9e48eb3d3178955b4e0
shreyansh <shreyansh9588@gmail.com>
2023-08-06 12:08:43 -0400

    Simple CRUD operations using Hibernate JPA

---
+++ src/main/java/com/sj/course/entity/Student.java
@@ -0,0 +1,40 @@
+package com.sj.course.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "student")
+public class Student {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  @Column(length = 36, nullable = false)
+  private UUID id;
+
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
+
+  @Column(name = "email_address", nullable = false, unique = true)
+  private String email;
+
+}
